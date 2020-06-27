4e2394f4f5b6bfecb1625918f30890a77125559a
Elkantor <vic03@live.fr>
2020-06-27 13:37:58 +0200

    Update main.c to load, show and unload world 1 data

--- code/main.c
+++ code/main.c
@@ -1,1 +1,31 @@
-#include "raylib.h"
+#include "dependencies/raylib.h"
+#include "config.h"
+#include "ui/fps_counter.c"
+#include "environments/world_1.c"
+
+int main(int argc, char** argv){
+    InitWindow(screen_width, screen_height, "rpg");
+    SetTargetFPS(60);
+
+    world_1_load();
+
+    while(!WindowShouldClose()){
+        // Inputs
+
+        // Logic by component
+
+        // Logic global
+
+        // Render
+        BeginDrawing();
+            ClearBackground(WHITE);
+            world_1_show(screen_ratio);
+            DrawFPS(fps_counter_x_get(screen_width, 24), fps_counter_y_get(24));
+        EndDrawing();
+    }
+
+    world_1_unload();
+
+    CloseWindow();
+    return 0;
+}
