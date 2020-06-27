6bd9a4d0db03747566a9f22ee2620730f43eb102
Elkantor <vic03@live.fr>
2020-06-27 13:31:45 +0200

    Add default world environment to be able to load the first level, and do stuff quickly

---
+++ code/environments/world_1.c
@@ -0,0 +1,33 @@
+int world_1_current_block = 0;
+Texture2D world_1_background_0_texture;
+Texture2D world_1_foreground_0_texture;
+
+void world_1_load(){
+    world_1_background_0_texture = LoadTexture("../data/environments/world_1/background_0.png");
+    world_1_foreground_0_texture = LoadTexture("../data/environments/world_1/foreground_0.png");
+}
+
+void world_1_unload(){
+    UnloadTexture(world_1_background_0_texture);
+    UnloadTexture(world_1_foreground_0_texture);
+}
+
+void world_1_show(float screen_ratio){
+    int scale = 2;
+    DrawTextureEx(world_1_background_0_texture, (Vector2){0, 0}, 0.f, 2.f * screen_ratio, WHITE);
+    DrawTextureEx(world_1_background_0_texture, (Vector2){world_1_background_0_texture.width*scale, 0}, 0.f, scale * screen_ratio, WHITE);
+    DrawTextureEx(world_1_background_0_texture, (Vector2){world_1_background_0_texture.width*scale*2, 0}, 0.f, scale * screen_ratio, WHITE);
+    DrawTextureEx(world_1_background_0_texture, (Vector2){world_1_background_0_texture.width*scale*3, 0}, 0.f, scale * screen_ratio, WHITE);
+
+    DrawTextureEx(world_1_background_0_texture, (Vector2){0, world_1_background_0_texture.height*scale}, 0.f, scale * screen_ratio, WHITE);
+    DrawTextureEx(world_1_background_0_texture, (Vector2){world_1_background_0_texture.width*scale, world_1_background_0_texture.height*scale}, 0.f, scale * screen_ratio, WHITE);
+    DrawTextureEx(world_1_background_0_texture, (Vector2){world_1_background_0_texture.width*scale*2, world_1_background_0_texture.height*scale}, 0.f, scale * screen_ratio, WHITE);
+    DrawTextureEx(world_1_background_0_texture, (Vector2){world_1_background_0_texture.width*scale*3, world_1_background_0_texture.height*scale}, 0.f, scale * screen_ratio, WHITE);
+
+    DrawTextureEx(world_1_background_0_texture, (Vector2){0, world_1_background_0_texture.height*scale*2}, 0.f, scale * screen_ratio, WHITE);
+    DrawTextureEx(world_1_background_0_texture, (Vector2){world_1_background_0_texture.width*scale, world_1_background_0_texture.height*scale*2}, 0.f, scale * screen_ratio, WHITE);
+    DrawTextureEx(world_1_background_0_texture, (Vector2){world_1_background_0_texture.width*scale*2, world_1_background_0_texture.height*scale*2}, 0.f, scale * screen_ratio, WHITE);
+    DrawTextureEx(world_1_background_0_texture, (Vector2){world_1_background_0_texture.width*scale*3, world_1_background_0_texture.height*scale*2}, 0.f, scale * screen_ratio, WHITE);
+
+    DrawTextureEx(world_1_foreground_0_texture, (Vector2){50, 50}, 0.f, 2.f * screen_ratio, WHITE);
+}
