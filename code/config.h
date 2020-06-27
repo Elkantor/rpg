fa4e786ff7ab546ca7b3e5800a24f1232520239d
Elkantor <vic03@live.fr>
2020-06-27 17:45:42 +0200

    Change screen_resolution naming variable to be able to know that it is an index

--- code/config.h
+++ code/config.h
@@ -1,6 +1,6 @@
 int screen_widths[] = { 1280,   1920,    2560 };
 int screen_heights[] = { 720,    1080,    1440 };
-int screen_resolution = 0;
+int screen_resolution_index = 0;
 float screen_ratio = 1280/720;
-#define screen_width (screen_widths[screen_resolution])
-#define screen_height (screen_heights[screen_resolution])
+#define screen_width (screen_widths[screen_resolution_index])
+#define screen_height (screen_heights[screen_resolution_index])
