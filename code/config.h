cade00cb386264fc00438e8f376cc467ec6282c5
SlipNinja <pierre_lanneau@hotmail.fr>
2020-06-27 16:01:20 +0200

    Change res to 1920

--- code/config.h
+++ code/config.h
@@ -1,6 +1,6 @@
 int screen_widths[] = { 1280,   1920,    2560 };
 int screen_heights[] = { 720,    1080,    1440 };
-int screen_resolution = 0;
+int screen_resolution = 1;
 float screen_ratio = 1280/720;
 #define screen_width (screen_widths[screen_resolution])
 #define screen_height (screen_heights[screen_resolution])
