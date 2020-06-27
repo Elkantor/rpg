0852fba60550a0728e5a0ef0f7c38d2e9bd68a81
Elkantor <vic03@live.fr>
2020-06-27 18:55:19 +0200

    Draw DEBUG MODE in text in the top of the screen to know if we currently are in debug mode or not

--- code/main.c
+++ code/main.c
@@ -4,6 +4,7 @@
 #include "ui/grid.c"
 #include "environments/world_1.c"
 
+
 int main(int argc, char** argv){
     InitWindow(screen_width, screen_height, "rpg");
     SetTargetFPS(60);
@@ -12,6 +13,9 @@ int main(int argc, char** argv){
 
     while(!WindowShouldClose()){
         // Inputs
+#if defined(DEBUG)
+
+#endif
 
         // Logic by component
 
@@ -23,6 +27,9 @@ int main(int argc, char** argv){
             world_1_show(screen_ratio);
             DrawFPS(fps_counter_x_get(screen_width, 24), fps_counter_y_get(24));
             grid_show(screen_width, screen_height, screen_cell_size);
+#if defined(DEBUG)
+            DrawText("DEBUG MODE", screen_width/2 - (4*24), 24, 24, RED);
+#endif
         EndDrawing();
     }
 
